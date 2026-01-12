afcc4671fd1814d71a9ea0417bc4dadd464794e2
AbdulRehman76 <abdulrehman208000@gmail.com>
2026-01-12 08:48:46 +0500

    Fix backend structure for deployment

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,7 @@ async function addUser() {
   const age = document.getElementById("age").value;
   const weight = document.getElementById("weight").value;
     console.log("add");
-    await fetch("http://localhost:4000/api/users", {
+    await fetch("https://crud-application-production-f8f4.up.railway.app/api/users/", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, age, weight })
@@ -14,7 +14,8 @@ async function addUser() {
 
 async function getUsers() {
   console.log("Get Script");
-  const res = await fetch("http://localhost:4000/api/users");
+  const res = await fetch("https://crud-application-production-f8f4.up.railway.app/api/users/");
+  // const res = await fetch("http://localhost:4000/api/users");
   const users = await res.json();
 
   const list = document.getElementById("list");
